ad2cead0768f03275496a8c3b8678357292cc6f7
SyahrialHipdi <91787760+SyahrialHipdi@users.noreply.github.com>
2026-04-30 20:39:49 +0700

    Update main.ino

--- Modul3/SourceCode/3.6.4/main.ino
+++ Modul3/SourceCode/3.6.4/main.ino
@@ -1,1 +1,53 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Arduino.h>
 
+// Ganti alamat jika perlu (Ox27 / Ox20) 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int pinPot = A0;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  int nilai = analogRead(pinPot);
+  float volt = nilai * (5.0 / 1023.0);
+  float persen = nilai * (100.0 / 1023.0);
+
+  // Mapping ke bar (0-16)
+  int panjangBar = map(nilai, 0, 1023, 0, 16);
+
+  // Tampilkan ke Serial
+  Serial.print("Nilai ADC: ");
+  Serial.print(nilai);
+  Serial.print(" | ");
+  Serial.print("Volt: ");
+  Serial.print(volt);
+  Serial.print(" | ");
+  Serial.print("Persen: ");
+  Serial.print(persen);
+  Serial.println("");
+
+  // Baris 1: nilai ADC
+  lcd.setCursor(0, 0);
+  lcd.print("ADC: ");
+  lcd.print(nilai);
+  lcd.print(" "); // clear sisa
+
+  // Baris 2: bar
+  lcd.setCursor(0, 1);
+  for (int i = 0; i < 16; i++) {
+    if (i < panjangBar) {
+      lcd.print((char)255);
+    }
+    else {
+    lcd.print(" ");
+    }
+  }
+  delay(200);
+
+}
