b1fe25bde96a681f91a03a98fa74f517c4ceb3f8
SyahrialHipdi <91787760+SyahrialHipdi@users.noreply.github.com>
2026-05-18 15:31:20 +0700

    Initialize and control left and right LEDs

--- Modul1/Source Code/1.6.4/main.ino
+++ Modul1/Source Code/1.6.4/main.ino
@@ -1,1 +1,39 @@
+int timer = 500;
 
+// LED kiri
+int kiri[] = {2, 3, 4};
+
+// LED kanan
+int kanan[] = {5, 6, 7};
+
+void setup()
+{
+  // Inisialisasi LED kiri
+  for (int i = 0; i < 3; i++) {
+    pinMode(kiri[i], OUTPUT);
+  }
+
+  // Inisialisasi LED kanan
+  for (int i = 0; i < 3; i++) {
+    pinMode(kanan[i], OUTPUT);
+  }
+}
+
+void loop()
+{
+  // Menyalakan LED kiri
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(kiri[i], HIGH);
+    digitalWrite(kanan[i], LOW);
+  }
+
+  delay(timer);
+
+  // Menyalakan LED kanan
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(kiri[i], LOW);
+    digitalWrite(kanan[i], HIGH);
+  }
+
+  delay(timer);
+}
